52f5e5190b24909b6626dc3f0591672bd03890f0
ah-aydin <ah-aydin@users.noreply.github.com>
2020-03-14 14:05:30 +0200

    Score system
    Added score system and UI

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 
 
+import os
 import pygame
 import random as rnd
-from color import GREEN, WHITE, RED, BLACK
 from tkinter import messagebox
+
 import snake as S
 from snake import Snake, Cube
+from color import GREEN, WHITE, RED, BLACK
+
+# Initialize required pygame modules
+pygame.init()
+pygame.font.init()
 
 # Enviroment variables
 top_offset = 50
 size = 600
 n_rows = 20
 dist = size // n_rows
+
+# Score related variables
+score = 0
+score_font = pygame.font.SysFont('score_font', 50)
 
 # Set the enviroment variables on snake
 S.set_env_variables(top_offset, size, dist)
@@ -20,9 +30,15 @@ game_is_running = True
 global snack
 global has_snack
 
-pygame.init()
-
 window = pygame.display.set_mode((size+1, size + top_offset+1))
+
+def update_score():
+    global score
+    score += 1
+
+def display_score():
+    score_surface = score_font.render('Score: {0}'.format(score), False, WHITE)
+    window.blit(score_surface, (5, 5))
 
 def check_collisions():
     check_collision()
@@ -34,7 +50,17 @@ def check_collision():
         if cube == head: continue
         if head.pos == cube.pos:
             pygame.quit()
-            messagebox.showinfo("GAME OVER", "GAME OVER\nTotal score: BANANAS")
+            high_score = -1
+            if(os.path.exists('high_score.txt')):
+                file = open('high_score.txt', 'r')
+                high_score = int(file.read())
+                file.close()
+            if score > high_score:
+                file = open('high_score.txt', 'w')
+                file.write(str(score))
+                messagebox.showinfo('GAME OVER', 'GAME OVER\n NEW HIGH SCORE \n\t{}\t'.format(score))
+            else:
+                messagebox.showinfo("GAME OVER", "GAME OVER\n TOTAL SCORE: {} \n HIGH SCORE: {}".format(score, high_score))
             exit()
 
 def check_snack_collision():
@@ -42,6 +68,7 @@ def check_snack_collision():
     global has_snack
     if snake.head.pos == snack.pos:
         snake.add_cube()
+        update_score()
         del snack
         # This solves a bug
         snack = Cube((-100, -100))
@@ -51,11 +78,18 @@ def spawn_snack(surface):
     global snack
     global has_snack
     has_snack = True
-    x = rnd.randint(0, n_rows-1)
-    while x == snake.pos[1] // n_rows:
-        return
-    y = rnd.randint(0, n_rows-1)
-    snack_start_pos = (x*dist, y*dist + top_offset)
+    snack_start_pos = (0,0)
+    in_snake = True
+    while True:
+        x = rnd.randint(0, n_rows-1)
+        y = rnd.randint(0, n_rows-1)
+        snack_start_pos = (x*dist, y*dist + top_offset)
+        in_snake = False
+        for c in snake.body:
+            if c.pos == snack_start_pos:
+                in_snake = True
+        if in_snake == False:
+            break
     snack = Cube(snack_start_pos, 0, 0, RED)
 
 def draw_frame(surface):
@@ -63,6 +97,7 @@ def draw_frame(surface):
     surface.fill(BLACK)
     snake.draw(surface)
     snack.draw(surface)
+    display_score()
     draw_grid(surface)
     pygame.display.update()
 
